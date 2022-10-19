2248dc97ce270a652c265ef352673bbd1e5394f1
AlexLenk <alexlenk.ru@gmail.com>
2022-10-19 07:12:43 +0300

    Created task 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import './index.css'
+import imgEL from './assets/achievements.png';
+
 console.log('Hello World!');
+
+const app = document.querySelector('#app')
+const imgEl = document.createElement('img')
+imgEl.classList.add('media')
+imgEl.src = imgEL
+app.append(imgEl)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,44 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const distPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'index.js'),
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: distPath,
     clean: true
   },
-};
+  devServer: {
+    static: distPath,
+    port: 3572
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+}
